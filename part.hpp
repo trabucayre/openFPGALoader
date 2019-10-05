1afeb2247814898bffc815c35dad61b1ef1cc59a
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-10-05 18:10:04 +0200

    part: fix correct model for artix 35

--- part.hpp
+++ part.hpp
@@ -11,7 +11,7 @@ typedef struct {
 } fpga_model;
 
 static std::map <int, fpga_model> fpga_list = {
-	{0x0362D093, {"xilinx", "artix a7 35t", "XC7"}},
+	{0x0362D093, {"xilinx", "artix a7 35t", "xc7a35"}},
 	{0x020f30dd, {"altera", "cyclone 10 LP", "10CL025"}}
 };
 
