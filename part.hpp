cc71db6b65810afc25234312093474c08a6566e5
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-12-06 07:27:46 +0100

    part: add GW1NR-9 FPGA

--- part.hpp
+++ part.hpp
@@ -14,6 +14,7 @@ static std::map <int, fpga_model> fpga_list = {
 	{0x0362D093, {"xilinx", "artix a7 35t", "xc7a35"}},
 	{0x020f30dd, {"altera", "cyclone 10 LP", "10CL025"}},
 	{0x612bd043, {"lattice", "MachXO3LF", "LCMX03LF-6900C"}},
+	{0x1100581b, {"Gowin", "GW1NR-9", "GW1N"}},
 };
 
 #endif
