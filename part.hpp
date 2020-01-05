b95a34403b9d2df03eec5cd06c1c43a0ea9430d1
Fabien Marteau <fabien.marteau@armadeus.com>
2020-01-05 15:09:11 +0100

    adding ID for gowin tangnano

--- part.hpp
+++ part.hpp
@@ -15,6 +15,7 @@ static std::map <int, fpga_model> fpga_list = {
 	{0x020f30dd, {"altera", "cyclone 10 LP", "10CL025"}},
 	{0x612bd043, {"lattice", "MachXO3LF", "LCMX03LF-6900C"}},
 	{0x1100581b, {"Gowin", "GW1N", "GW1NR-9"}},
+	{0x0900281B, {"Gowin", "GW1N1", "GW1N-1"}},
 };
 
 #endif
