30552bf01db9ff9e3726cdd6004ab2f23ef9ddfd
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2020-01-31 22:14:48 +0100

    part: add support for LFE5UM5G-85F

--- src/part.hpp
+++ src/part.hpp
@@ -14,6 +14,7 @@ static std::map <int, fpga_model> fpga_list = {
 	{0x0362D093, {"xilinx", "artix a7 35t", "xc7a35"}},
 	{0x020f30dd, {"altera", "cyclone 10 LP", "10CL025"}},
 	{0x612bd043, {"lattice", "MachXO3LF", "LCMX03LF-6900C"}},
+	{0x81113043, {"lattice", "ECP5-5G", "LFE5UM5G-85F-8BG381"}},
 	{0x1100581b, {"Gowin", "GW1N", "GW1NR-9"}},
 	{0x0900281B, {"Gowin", "GW1N", "GW1N-1"}},
 	{0x0100381B, {"Gowin", "GW1N", "GW1N-4"}},
