03769be93795705e1d7a6e99ac957e1c80d77c41
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2022-03-03 15:35:29 +0100

    spiOverJtag: Xilinx Spartan6 LX150T

--- spiOverJtag/build.py
+++ spiOverJtag/build.py
@@ -54,6 +54,7 @@ if tool in ["ise", "vivado"]:
         "xc6slx16csg324"   : "xc6s_csg324",
         "xc6slx45csg324"   : "xc6s_csg324",
         "xc6slx100fgg484"  : "xc6s_fgg484",
+        "xc6slx150tfgg484" : "xc6s_fgg484",
         "xc7a35tcpg236"    : "xc7a_cpg236",
         "xc7a35tcsg324"    : "xc7a_csg324",
         "xc7a35tftg256"    : "xc7a_ftg256",
@@ -75,12 +76,14 @@ if tool in ["ise", "vivado"]:
                             "xc6slx16ftg256": "xc6slx16",
                             "xc6slx16csg324": "xc6slx16",
                             "xc6slx45csg324": "xc6slx45",
-                            "xc6slx100fgg484": "xc6slx100"}[part],
+                            "xc6slx100fgg484": "xc6slx100",
+                            "xc6slx150tfgg484": "xc6slx150t"}[part],
                         'package': {
                             "xc6slx16ftg256": "ftg256",
                             "xc6slx16csg324": "csg324",
                             "xc6slx45csg324": "csg324",
-                            "xc6slx100fgg484": "fgg384"}[part],
+                            "xc6slx100fgg484": "fgg384",
+                            "xc6slx150tfgg484": "fgg484"}[part],
                         'speed' : -3
                 }
     else:
