74eda1cd8ad6a7a8a920df7816fe882cbb92e37b
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2022-05-10 19:07:56 +0200

    spiFlashdb: N25Q64 part

--- src/spiFlashdb.hpp
+++ src/spiFlashdb.hpp
@@ -99,6 +99,19 @@ static std::map <uint32_t, flash_t> flash_list = {
 		.bp_len = 3,
 		.bp_offset = {(1 << 2), (1 << 3), (1 << 4), 0}}
 	},
+	{0x0020ba17, {
+		.manufacturer = "micron",
+		.model = "N25Q64",
+		.nr_sector = 128,
+		.sector_erase = true,
+		.subsector_erase = true,
+		.has_extended = true,
+		.tb_otp = false,
+		.tb_offset = (1 << 5),
+		.tb_register = STATR,
+		.bp_len = 4,
+		.bp_offset = {(1 << 2), (1 << 3), (1 << 4), (1 << 6)}}
+	},
 	{0x0020ba18, {
 		.manufacturer = "micron",
 		.model = "N25Q128",
