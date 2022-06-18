2f30668dbc9988e647e6b9f276f69383c85d4894
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2022-06-18 22:55:58 +0200

    ORBTrace Mini support (DFU mode)

--- src/board.hpp
+++ src/board.hpp
@@ -172,6 +172,7 @@ static std::map <std::string, target_board_t> board_list = {
 	JTAG_BOARD("tangnano4k",      "", "ft2232",     0, 0, CABLE_DEFAULT),
 	JTAG_BOARD("tangnano9k",      "", "ft2232",     0, 0, CABLE_DEFAULT),
 	JTAG_BOARD("tec0117",         "", "ft2232",     0, 0, CABLE_DEFAULT),
+	DFU_BOARD("orbtrace_dfu",     "", "dfu", 0x1209, 0x3442, 1),
 	JTAG_BITBANG_BOARD("ulx2s",   "", "ft232RL", 0, 0,
 			FT232RL_RI, FT232RL_DSR, FT232RL_CTS, FT232RL_DCD, CABLE_DEFAULT),
 	JTAG_BITBANG_BOARD("ulx3s",   "", "ft231X",  0, 0,
