9e2edeb6c188eccd5e7d71f07466d227a086aaf5
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-02-26 21:15:36 +0100

    spiOverJtag: added xc7a15tcpg236 bitstream

--- spiOverJtag/build.py
+++ spiOverJtag/build.py
@@ -73,6 +73,7 @@ if tool in ["ise", "vivado"]:
         "xc6slx100fgg484"  : "xc6s_fgg484",
         "xc6slx150tcsg484" : "xc6s_csg484",
         "xc6slx150tfgg484" : "xc6s_fgg484",
+        "xc7a15tcpg236"    : "xc7a_cpg236",
         "xc7a25tcpg238"    : "xc7a_cpg238",
         "xc7a25tcsg325"    : "xc7a_csg325",
         "xc7a35tcpg236"    : "xc7a_cpg236",
