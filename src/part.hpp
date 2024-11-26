daa91c9a41a777f232d1d274783a3c0eeffae684
Florent Kermarrec <florent@enjoy-digital.fr>
2024-11-26 16:02:28 +0100

    src/part.hpp: Add Altera Max10 10M16SAU169C8G support (Used by LimeSDR Mini V1).

--- src/part.hpp
+++ src/part.hpp
@@ -204,9 +204,10 @@ static std::map <uint32_t, fpga_model> fpga_list = {
 	{0x02d120dd, {"altera", "cyclone V Soc", "5CSE*A5/5CST*5/5CSX*5", 10}},
 
 	/* Altera Max 10 */
-	{0x031820dd, {"altera", "MAX 10", "10M08SAU169C8G", 10}},
-	{0x031050dd, {"altera", "MAX 10", "10M50DAF484",    10}},
+	{0x031820dd, {"altera", "MAX 10", "10M08SAU169C8G",    10}},
+	{0x031050dd, {"altera", "MAX 10", "10M50DAF484",       10}},
 	{0x0318d0dd, {"altera", "MAX 10", "10M40SCE144C8G",    10}},
+	{0x031830dd, {"altera", "MAX 10", "10M16SAU169C8G",    10}},
 
 	/* Altera Cyclone 10 */
 	{0x020f30dd, {"altera", "cyclone 10 LP", "10CL025", 10}},
