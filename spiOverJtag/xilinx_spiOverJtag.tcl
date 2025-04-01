fdf24a824e7b45eac18fa3f36f0c77b5d615942e
Neo <neo@example.com>
2025-04-01 22:19:18 +0300

    add xc7a50tcsg325

--- spiOverJtag/xilinx_spiOverJtag.tcl
+++ spiOverJtag/xilinx_spiOverJtag.tcl
@@ -13,6 +13,7 @@ set grade [dict create \
 	xc7a35tcsg324  -1 \
 	xc7a35tftg256  -1 \
 	xc7a50tcpg236  -2 \
+	xc7a50tcsg325  -2 \
 	xc7a75tfgg484  -2 \
 	xc7a100tfgg484 -2 \
 	xc7a200tsbg484 -1 \
@@ -31,6 +32,7 @@ set pkg_name [dict create \
 	xc7a35tcsg324  xc7a_csg324 \
 	xc7a35tftg256  xc7a_ftg256 \
 	xc7a50tcpg236  xc7a_cpg236 \
+	xc7a50tcsg325  xc7a_csg325 \
 	xc7a75tfgg484  xc7a_fgg484 \
 	xc7a100tfgg484 xc7a_fgg484 \
 	xc7a200tsbg484 xc7a_sbg484 \
